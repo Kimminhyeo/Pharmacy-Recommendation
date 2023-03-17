7f9007b0062071707de7afad15a717fad6e98f41
Kimminhyeo <ualsgur99@gmail.com>
2023-03-17 21:29:44 +0900

    Spring retry 구현 및 검증

---
+++ src/main/java/com/min/pharmacyrecommendation/config/RetryConfig.java
@@ -0,0 +1,11 @@
+package com.min.pharmacyrecommendation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+
+}
